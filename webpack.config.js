4e95f5861af771a65b636dbed1ccd654f72ad36c
vankai <625608852@qq.com>
2017-09-29 23:15:44 +0800

    resolve react@16 cross-origin error handling

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-source-map',
 
   entry: {
     vendor: [
